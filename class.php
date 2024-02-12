cdeaff5d6e859220901659f345b3bb8ee6a21d5e
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-02-13 00:18:50 +0600

    php oop intro

---
+++ class.php
@@ -0,0 +1,21 @@
+
+<?php
+
+class Applephone {
+
+   public $display = 'AMOLED';
+   public $RAM     = 12;
+   public $ROM     = 4;
+
+   public function color (){
+
+   }
+}
+
+class Car {
+
+    public function owner(){
+        echo 'Abdur Rahim';
+    }
+}
+?>
